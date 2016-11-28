2bbc3e49011a425b0c1d6e20bb015bbfe522eac4
helen77 <zny_ok1989@hotmail.com>
2016-11-28 03:04:29 +0000

    changed test_stack.cc

--- test/test_stack.cc
+++ test/test_stack.cc
@@ -4,11 +4,16 @@
 int main(void) {
     Stack s = Stack();
     SValue val;
-
     std::cout << "Putting items on the stack: 1,2,3" << std::endl;
+	try{
     s.push(1);
     s.push(2);
     s.push(3);
+	}catch(const char* msg) {
+      std::cout << msg << std::endl;
+   }
+
+	try{
     std::cout << "Popping items off the stack and printing:" << std::endl;
     val = s.pop();
     print(val);
@@ -21,12 +26,19 @@ int main(void) {
     std::cout << std::endl;
     val = s.pop();
     print(val);
+	}catch(const char* msg) {
+      std::cout << msg << std::endl;
+   }
     std::cout << std::endl;
+	try{
     std::cout << "Putting items on the stack: 4,5,6" << std::endl;
     s.push(4);
     s.push(5);
     s.push(6);
+	}catch(const char* msg) {
+      std::cout << msg << std::endl;
+   }
     std::cout << "Printing the stack:" << std::endl;
-    //s.print();
+    s.print();
     return 0;
 }
